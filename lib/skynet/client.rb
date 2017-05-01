63136defd4f4bf36b0d50837062817a73e5ca5ad
ElliottAYoung <elliott.a.young@gmail.com>
2017-05-01 17:07:42 -0500

    set Skynet::Client to be primary interface

--- lib/skynet/client.rb
+++ lib/skynet/client.rb
@@ -1,7 +1,20 @@
-require "skynet/client/version"
+require "skynet/accessor"
+require "skynet/splitter"
+require "skynet/responder"
+require "skynet/response"
 
 module Skynet
-  module Client
-    # Your code goes here...
+  class Client
+    attr_reader :access_code, :skynet_url
+
+    def initialize(args = {})
+      @access_code = args[:access_code]
+      @skynet_url = args[:skynet_url]
+    end
+
+    def method_missing(*args)
+      response = Skynet::Accessor.query(self, args[0], args[1])
+      Skynet::Responder.parse(response)
+    end
   end
 end
