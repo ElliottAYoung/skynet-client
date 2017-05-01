f9ccaebd870c7d3c1b6c7cdd463083907d63c18b
ElliottAYoung <elliott.a.young@gmail.com>
2017-05-01 15:50:23 -0500

    Initial Commit

---
+++ lib/skynet/client.rb
@@ -0,0 +1,7 @@
+require "skynet/client/version"
+
+module Skynet
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ lib/skynet/client/version.rb
@@ -0,0 +1,5 @@
+module Skynet
+  module Client
+    VERSION = "0.1.0"
+  end
+end
