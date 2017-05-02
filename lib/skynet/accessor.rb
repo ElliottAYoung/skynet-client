38d6f11000018c1d4ea8574e6d3f85c17c5a517f
ElliottAYoung <elliott.a.young@gmail.com>
2017-05-02 13:17:29 -0500

    Remove pry

--- lib/skynet/accessor.rb
+++ lib/skynet/accessor.rb
@@ -24,7 +24,6 @@ module Skynet
       def access_skynet(path_info, args)
         method, path = Skynet::Splitter.clean(path_info)
         method, path, args = Skynet::Identifier.identify(method, path, args)
-        binding.pry
 
         @conn.send(method, *[path, args])
       end
